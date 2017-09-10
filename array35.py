2a786f8be1e2a5a6a1fc36baf5b738a32281c8be
YULEI <yuleichin@126.com>
2017-09-10 19:13:49 +0800

    add array35

--- array35.py
+++ array35.py
@@ -9,4 +9,22 @@
 @file: array35.py 
 @time: 10/09/2017 16:24 
 """
-
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # 这题也主要是考察对于newTail的使用，前提是有序数组
+        if not nums:
+            return 0
+        newTail = 0
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+            elif nums[i] < target:
+                newTail += 1
+            else:
+                return newTail
+        return newTail
